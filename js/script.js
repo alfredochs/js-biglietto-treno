377107e3783b2477ad26edab3218f22ef00bc09f
alfredochs <chanta.alfredo@gmail.com>
2021-10-08 17:11:15 +0200

    add &&

--- js/script.js
+++ js/script.js
@@ -1,38 +1,22 @@
-// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi
-//  sul prezzo). Questo richiederà un minimo di ricerca.
-
-
-
-const etaPassegero = prompt("Quanti anni hai?")
-const numeroKm = prompt("Quanti km vuoi percorrere oggi?")
+const numeroKm = parseInt(prompt("Quanti km vuoi percorrere oggi?"))
+const etaPassegero = parseInt(prompt("Quanti anni hai?"))
 const prezzoBiglietto = numeroKm * 0.21
 let scontoMinorenni
 let scontoOver
 
+if ((isNaN(numeroKm)) && (isNaN(etaPassegero))) {
+    alert("I dati inseriti non sono corretti.")
+} else { }
 
 if (etaPassegero < 18) {
-    alert("Hai diritto ad uno sconto del 20%")
-} else if (etaPassegero >= 65) {
-    alert("Hai diritto d uno scondo del 40%")
-} else {
-    alert("Il prezzo del biglietto è: " + prezzoBiglietto)
-}
-
-
-if (etaPassegero < 18) {
-    scontoMinorenni = (prezzoBiglietto * 20 / 100)
+    scontoMinorenni = prezzoBiglietto * 20 / 100
     const prezzoScontoMinorenni = prezzoBiglietto - scontoMinorenni
-    alert("Il prezzo del viagio Minorenni è: " + (prezzoScontoMinorenni).toFixed(2))
+    alert("Hai diritto ad uno sconto del 20%")
+    alert("Il prezzo del viaggio Minorenni è: " + (prezzoScontoMinorenni).toFixed(2))
 } else if (etaPassegero >= 65) {
     scontoOver = prezzoBiglietto * 40 / 100
     const prezzoScontoOver = prezzoBiglietto - scontoOver
-    alert("Il prezzo del viagio Over 65 è: " + (prezzoScontoOver).toFixed(2))
+    alert("Hai diritto d uno scondo del 40%")
+    alert("Il prezzo del viaggio Over 65 è: " + (prezzoScontoOver).toFixed(2))
 } else {
 }
-
-
